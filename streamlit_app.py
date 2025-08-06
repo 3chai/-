2302b89e870f9be3e231bd1ac1d32c8f2dd71610
3chai <m.ur.yuki.mur@gmail.com>
2025-08-06 17:19:43 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ presets = {
     "推しの子": {
         "first_frame_top_y_true": 468,
         "frame_height_true": 27.3,
-        "cell_x_positions_true": {cell: 52 + 29 * offset for cell, offset in cell_offsets.items()},
+        "cell_x_positions_true": {cell: 51.7 + 29 * offset for cell, offset in cell_offsets.items()},
         "column_offset_x": 870,
         "true_width": 1754,
         "true_height": 2480

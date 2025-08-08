516185d838e2b1e3e33e5b6e2d341ac10aa333f5
3chai <m.ur.yuki.mur@gmail.com>
2025-08-08 17:40:29 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -334,7 +334,7 @@ def generate_timesheet(file_bytes, preset, show_books=True, book_offset_koma=6, 
                         items.append((n, s))
                     items.sort(key=lambda t: t[0])
 
-                    line_gap = 3*scale_h
+                    line_gap = 4*scale_h
                     margin   = 12*scale_w
                     bottom_label_bottom = None
 

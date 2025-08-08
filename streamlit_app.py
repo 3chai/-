994cc1b43d9411e597413046ba861da88f578d7a
3chai <m.ur.yuki.mur@gmail.com>
2025-08-08 19:59:04 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ presets = {
     "動画工房": {
         "first_frame_top_y_true": 468,
         "frame_height_true": 27.25,
-        "cell_x_positions_true": {cell: 51.7 + 29　　 * offset for cell, offset in cell_offsets.items()},
+        "cell_x_positions_true": {cell: 51.7 + 29 * offset for cell, offset in cell_offsets.items()},
         "column_offset_x": 870,
         "true_width": 1754,
         "true_height": 2480,

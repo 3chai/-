a5ae7a2a0bc19c6ac362f3c4b2d43041f904c40a
3chai <m.ur.yuki.mur@gmail.com>
2025-09-05 01:01:27 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ presets = {
         "first_frame_top_y_true": 800,                  # 最初のフレームの上端Y
         "frame_height_true": 27.5,                      # 1コマの高さ
         "cell_x_positions_true": {cell: 86 + 30.8 * offset for cell, offset in cell_offsets.items()},
-        "column_offset_x": 981.8        ,                         # 右カラムまでのXオフセット
+        "column_offset_x": 950.5,                         # 右カラムまでのXオフセット
         "true_width": 2026,
         "true_height": 2866,
         "default_book_koma": 4,
@@ -42,7 +42,7 @@ presets = {
         "first_frame_top_y_true": 779,                  # 最初のフレームの上端Y
         "frame_height_true": 33.93,                      # 1コマの高さ
         "cell_x_positions_true": {cell: 109 + 37 * offset for cell, offset in cell_offsets.items()},
-        "column_offset_x": 1125,
+        "column_offset_x": 1162,
         "true_width": 2340,
         "true_height": 3307,
         "default_book_koma": 4,

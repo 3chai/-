e465a4e35fb4bac4c5240d549ffa19e55d21c15b
3chai <m.ur.yuki.mur@gmail.com>
2025-09-05 00:09:14 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,8 +41,8 @@ presets = {
      "CygamesPictures": {
         "first_frame_top_y_true": 780,                  # 最初のフレームの上端Y
         "frame_height_true": 33.9,                      # 1コマの高さ
-        "cell_x_positions_true": {cell: 109.5 + 36.8 * offset for cell, offset in cell_offsets.items()},
-        "column_offset_x": 1121,
+        "cell_x_positions_true": {cell: 109 + 37 * offset for cell, offset in cell_offsets.items()},
+        "column_offset_x": 1125,
         "true_width": 2340,
         "true_height": 3307,
         "default_book_koma": 4,

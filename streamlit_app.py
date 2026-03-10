e0b31cf69003787fa93454e039cb84f8e9491070
3chai <m.ur.yuki.mur@gmail.com>
2026-03-10 16:11:05 +0900

    Adjust frame parameters for サイエンスSARU

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,9 +63,9 @@ presets = {
         "default_celllabel_koma": 2
     },
     "サイエンスSARU": {
-        "first_frame_top_y_true": 639,
-        "frame_height_true": 24 ,
-        "cell_x_positions_true": {cell: 39 + 30 * offset for cell, offset in cell_offsets.items()},
+        "first_frame_top_y_true": 638.5,
+        "frame_height_true": 24.5 ,
+        "cell_x_positions_true": {cell: 40 + 30 * offset for cell, offset in cell_offsets.items()},
         "column_offset_x": 855.5,
         "true_width": 1754,
         "true_height": 2480,

ed9d560b29623b6be42b522751f4239d3de06830
3chai <m.ur.yuki.mur@gmail.com>
2026-04-23 21:48:30 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,8 +35,8 @@ presets = {
     "Bibury": {
         "first_frame_top_y_true": 1310,
         "frame_height_true": 47.25,
-        "cell_x_positions_true": {cell: 100 + 54.5  * offset for cell, offset in cell_offsets.items()},
-        "column_offset_x": 1717,
+        "cell_x_positions_true": {cell: 102 + 54.5  * offset for cell, offset in cell_offsets.items()},
+        "column_offset_x": 1710,
         "true_width": 3508,
         "true_height": 4961,
         "default_book_koma": 5,

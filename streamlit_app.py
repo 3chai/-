08e54f15ddd549f20510c2b5095f0fd76d14b043
3chai <m.ur.yuki.mur@gmail.com>
2026-04-23 21:47:36 +0900

    Adjust cell_x_positions_true and column_offset_x values

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,8 +35,8 @@ presets = {
     "Bibury": {
         "first_frame_top_y_true": 1310,
         "frame_height_true": 47.25,
-        "cell_x_positions_true": {cell: 99 + 54.5  * offset for cell, offset in cell_offsets.items()},
-        "column_offset_x": 1720,
+        "cell_x_positions_true": {cell: 100 + 54.5  * offset for cell, offset in cell_offsets.items()},
+        "column_offset_x": 1717,
         "true_width": 3508,
         "true_height": 4961,
         "default_book_koma": 5,
